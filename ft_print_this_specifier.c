2639d18d18e388058c122990b53dca0ec6ff0dca
hsybassi <hsybassi@student.21-school.ru>
2021-11-09 16:48:00 +0200

    shortened the print_this_specifier function

--- ft_print_this_specifier.c
+++ ft_print_this_specifier.c
@@ -4,47 +4,26 @@
 
 int print_this_specifier(va_list list, char sp)
 {
-    int sp_len;
-    int n;
-    char *s;
-    void *p;
-    unsigned int ui;
+    int result_len;
 
     if (sp == 'c')
-    {
-        n = va_arg(list, int);
-        ft_putchar_fd(n, 1);
-    }
+        result_len = ft_putchar_fd(va_arg(list, int), 1);
     else if (sp == 's')
-    {
-        s = va_arg(list, char *);
-        ft_putstr_fd(s, 1);
-    }
+        result_len = ft_putstr_fd(va_arg(list, char *), 1);
     else if (sp == 'd' || sp == 'i') // правильно?
-    {
-        n = va_arg(list, int);
-        ft_putnbr_fd(n, 1);
-    }
+        result_len = ft_putnbr_fd(va_arg(list, int), 1);
     else if (sp == 'p')
-    {
-        p = va_arg(list, void *);
-        ft_putstr_fd(p, 1);
-    }
+        result_len = ft_putstr_fd(va_arg(list, void *), 1);
     else if (sp == 'u')
-    {
-        ui = va_arg(list, unsigned int);
-        ft_putnbr_fd(ui, 1);
-    }
+        result_len = ft_putnbr_fd(va_arg(list, unsigned int), 1);
     else if (sp == 'x')
-    {
-        ui = print_in_hexadecimal_lowercase(va_arg(list, int), 1);
-    }
+        result_len = print_in_hexadecimal_lowercase(va_arg(list, int), 1);
     else if (sp == 'X')
-    {
-        ui = print_in_hexadecimal_uppercase(va_arg(list, int), 1);
-    }
+        result_len = print_in_hexadecimal_uppercase(va_arg(list, int), 1);
     if (sp == '%')
     {
         ft_putchar_fd('%', 1);
+        result_len++;
     }
+    return (result_len);
 }
