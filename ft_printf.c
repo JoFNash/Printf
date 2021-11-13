7f71c89023e2232f147f958afba8ec4576093feb
Harp Sybassion <hsybassi@ox-q5.msk.21-school.ru>
2021-11-13 21:20:26 +0300

    where is the leak?

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hsybassi <hsybassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 16:53:40 by hsybassi          #+#    #+#             */
-/*   Updated: 2021/11/13 20:38:15 by hsybassi         ###   ########.fr       */
+/*   Updated: 2021/11/13 21:15:08 by hsybassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ int	ft_printf(const char	*format, ...)
 		format++;
 	}
 	va_end(list);
+	printf("length = %d\n", length);
 	return (length);
 }
--- main.c
+++ main.c
@@ -4,27 +4,27 @@
 
 int	main(void)
 {
-	printf("p:\n");
-	printf("1 %p\n", -1);
-	printf("2 %p \n", 1);
-	printf("3 %p \n", 15);
-	printf("4 %p \n", 16);
-	printf("5 %p \n", 17);
-	printf("6 %p %p \n", LONG_MIN, LONG_MAX);
-	printf("7 %p %p \n", INT_MIN, INT_MAX);
-	printf("8 %p %p \n", ULONG_MAX, -ULONG_MAX);
-	printf("9 %p %p \n", 0, 0);
+	// printf("p:\n");
+	// printf("1 %x\n", -1);
+	// printf("2 %x \n", 1);
+	// printf("3 %x \n", 15);
+	// printf("4 %x \n", 16);
+	// printf("5 %x \n", 17);
+	// printf("6 %x %x \n", LONG_MIN, LONG_MAX);
+	// printf("7 %x %x \n", INT_MIN, INT_MAX);
+	// printf("8 %x %x \n", ULONG_MAX, -ULONG_MAX);
+	// printf("9 %x %x \n", 0, 0);
 
 	ft_printf("p:\n");
-	ft_printf("1 %p\n", -1);
-	ft_printf("2 %p \n", 1);
-	ft_printf("3 %p \n", 15);
-	ft_printf("4 %p \n", 16);
-	ft_printf("5 %p \n", 17);
-	ft_printf("6 %p %p \n", LONG_MIN, LONG_MAX);
-	ft_printf("7 %p %p \n", INT_MIN, INT_MAX);
-	ft_printf("8 %p %p \n", ULONG_MAX, -ULONG_MAX);
-	ft_printf("9 %p %p \n", 0, 0);
+	ft_printf("1 %x\n", -1);
+	ft_printf("2 %x \n", 1);
+	ft_printf("3 %x \n", 15);
+	ft_printf("4 %x \n", 16);
+	ft_printf("5 %x \n", 17);
+	ft_printf("6 %x %x \n", LONG_MIN, LONG_MAX);
+	ft_printf("7 %x %x \n", INT_MIN, INT_MAX);
+	ft_printf("8 %x %x \n", ULONG_MAX, -ULONG_MAX);
+	ft_printf("9 %x %x \n", 0, 0);
 	// printf("\nx:\n");
 	// printf("1 %x\n", -1);
 	// printf("2 %x \n", 1);
@@ -36,15 +36,15 @@ int	main(void)
 	// printf("8 %x %x \n", ULONG_MAX, -ULONG_MAX);
 	// printf("9 %x %x \n", 0, 0);
 
-	// ft_printf(" %p ", -1);
-	// ft_printf(" %p ", 1);
-	// ft_printf(" %p ", 15);
-	// ft_printf(" %p ", 16);
-	// ft_printf(" %p ", 17);
-	// ft_printf(" %p %p ", LONG_MIN, LONG_MAX);
-	// ft_printf(" %p %p ", INT_MIN, INT_MAX);
-	// ft_printf(" %p %p ", ULONG_MAX, -ULONG_MAX);
-	// ft_printf(" %p %p ", 0, 0);
+	// ft_printf(" %x ", -1);
+	// ft_printf(" %x ", 1);
+	// ft_printf(" %x ", 15);
+	// ft_printf(" %x ", 16);
+	// ft_printf(" %x ", 17);
+	// ft_printf(" %x %x ", LONG_MIN, LONG_MAX);
+	// ft_printf(" %x %x ", INT_MIN, INT_MAX);
+	// ft_printf(" %x %x ", ULONG_MAX, -ULONG_MAX);
+	// ft_printf(" %x %x ", 0, 0);
 
 	return (0);
 }
